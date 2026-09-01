1ea01ce63b3630fb5d2cab077c7be5745d5aea92
unknown <SkYantS28>
2026-09-01 14:02:14 -0300

    Corrige caminhos das imagens

--- src/data/livros.js
+++ src/data/livros.js
@@ -3,139 +3,26 @@ export const livros = [
     titulo: "36 Tons do Baralho Cigano",
     autor: "Eliana Ferreira de Sousa",
     categoria: "Baralho Cigano",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/36_tons_do_baralho_cigano_eliana_ferreira_de_sousa.jpg",
+    imagem: `${import.meta.env.BASE_URL}images/livros/36_tons_do_baralho_cigano_eliana_ferreira_de_sousa.jpg`,
   },
-
   {
     titulo: "7 Day Lenormand",
     autor: "Andrea Green",
     categoria: "Lenormand",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/7_day_lenormand_andrea_green.jpg",
+    imagem: `${import.meta.env.BASE_URL}images/livros/7_day_lenormand_andrea_green.jpg`,
   },
-
   {
     titulo: "Apostila Curso de Baralho Cigano Profissional",
     autor: "Adrea Sá Alexandre",
     categoria: "Baralho Cigano",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/apostila_curso_de_baralho_cigano_profissional_adrea_sa_alexandre.jpg",
+    imagem: `${import.meta.env.BASE_URL}images/livros/apostila_curso_de_baralho_cigano_profissional_adrea_sa_alexandre.jpg`,
   },
-
   {
     titulo: "Aprendiz Lenormand",
     autor: "Charlie Normandel",
     categoria: "Lenormand",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/aprendiz_lenormand_charlie_normandel_ml_editora.png",
+    imagem: `${import.meta.env.BASE_URL}images/livros/aprendiz_lenormand_charlie_normandel_ml_editora.png`,
   },
 
-  {
-    titulo: "A Roda Astrológica com Petit Lenormand",
-    autor: "Diogenia Faria de Jesus",
-    categoria: "Lenormand",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/a_roda-astrologica_com_petit_lenormand_diogenia_faria_de_jesus.jpg",
-  },
-
-  {
-    titulo: "Baralho Cigano",
-    autor: "André Mantavanni",
-    categoria: "Baralho Cigano",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/baralho_cigano_andre_mantavanni.jpg",
-  },
-
-  {
-    titulo: "Baralho Cigano",
-    autor: "Geraldo Spacassassi",
-    categoria: "Baralho Cigano",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/baralho_cigano_geraldo_spacassassi.webp",
-  },
-
-  {
-    titulo: "Baralho Cigano Lenormand Tradicional",
-    autor: "Odete Lopes Mazza",
-    categoria: "Baralho Cigano",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/baralho_cigano_lenormand_tradicional_odete_lopes_mazza.jpg",
-  },
-
-  {
-    titulo: "Baralho Cigano - Livro Dinâmico da Vida",
-    autor: "Leandro Roque",
-    categoria: "Baralho Cigano",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/baralho_cigano_livro_dinamico_da_vida_leandro_roque.jpg",
-  },
-
-  {
-    titulo: "Baralho Cigano no Amor",
-    autor: "André Mantavanni",
-    categoria: "Baralho Cigano",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/baralho_cigano_no_amor_andre_mantavanni.jpg",
-  },
-
-  {
-    titulo: "Curso de Baralho Cigano",
-    autor: "Escola Esotérica Luz da Lua",
-    categoria: "Baralho Cigano",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/curso_de_baralho_cigano_escola_esoterica_luz_da_luaa.png",
-  },
-
-  {
-    titulo: "Descubra os Segredos de Lenormand",
-    autor: "Sofia Rito",
-    categoria: "Lenormand",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/descubra_os_segredos_de_lenormand_sofia_rito.jpg",
-  },
-
-  {
-    titulo: "Guia Prático Baralho Cigano",
-    autor: "Astral Total",
-    categoria: "Baralho Cigano",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/guia_pratico_baralho_cigano_astral_total.webp",
-  },
-
-  {
-    titulo: "Learning Lenormand",
-    autor: "Marcus Katz e Tali Goodwin",
-    categoria: "Lenormand",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/learning_lenormand_marcus_katz_e_tali_goodwin.jpg",
-  },
-
-  {
-    titulo: "Lenormand na Prática",
-    autor: "Odete Lopes Mazza",
-    categoria: "Lenormand",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/lenormand_na_pratica_odete_lopes_mazza.jpg",
-  },
-
-  {
-    titulo: "Método Alemão - Introdução às Combinações",
-    autor: "Odete Lopes Mazza",
-    categoria: "Lenormand",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/metodo_alemao_introducao_as_combinacoes_odete_lopes_mazza.jpg",
-  },
-
-  {
-    titulo: "Método Criativo de Leitura",
-    autor: "Vivian Lenormand",
-    categoria: "Lenormand",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/metodo_criativo_de_leitura_vivian_lenormand.jpg",
-  },
-
-  {
-    titulo: "O Livro Completo do Baralho Petit Lenormand",
-    autor: "Odete Lopes Mazza",
-    categoria: "Lenormand",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/o_livro_completo_do_baralho_petit_lenormand_odete_lopes-mazza.jpg",
-  },
-
-  {
-    titulo: "O Tempo no Baralho Cigano",
-    autor: "Vivia Lenormand",
-    categoria: "Baralho Cigano",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/o_tempo_no_baralho_cigano_vivia_lenormand.jpg",
-  },
-
-  {
-    titulo: "The Complete Lenormand Oracle Handbook",
-    autor: "Caitlin Matthews",
-    categoria: "Lenormand",
-    imagem: "`${import.meta.env.BASE_URL}/images/livros/the_complete_lenormand_oracle_handbook_caitlin_matthews.jpg",
-  },
+  // ... restante dos livros
 ];
